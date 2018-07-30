a91e96fb8eb208d58c34e4e01705da2e9829547c
unknown <1043736808@qq.com>
2018-07-30 18:07:45 +0800

    js

---
+++ 2-JS练习/JavaScript基础部分/JS10-日期对象、网页时钟、倒计时课件/miaov.js
@@ -0,0 +1,54 @@
+
+function doMove ( obj, attr, dir, target, endFn ) {
+	
+	dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
+	
+	clearInterval( obj.timer );
+	
+	obj.timer = setInterval(function () {
+		
+		var speed = parseInt(getStyle( obj, attr )) + dir;			// 步长
+		
+		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
+			speed = target;
+		}
+		
+		obj.style[attr] = speed + 'px';
+		
+		if ( speed == target ) {
+			clearInterval( obj.timer );
+			endFn && endFn();
+		}
+		
+	}, 30);
+}
+
+function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr] : getComputedStyle( obj )[attr]; }
+
+function shake ( obj, attr, endFn ) {
+	
+	if ( obj.onOff ) return;
+	obj.onOff = true;
+	
+	var pos = parseInt( getStyle(obj, attr) );			// 有隐患的
+	
+	var arr = [];			// 20, -20, 18, -18 ..... 0
+	var num = 0;
+	var timer = null;
+		
+	for ( var i=20; i>0; i-=2 ) {
+		arr.push( i, -i );
+	}
+	arr.push(0);
+		
+	clearInterval( obj.shake );
+	obj.shake = setInterval(function (){
+		obj.style[attr] = pos + arr[num] + 'px';
+		num++;
+		if ( num === arr.length ) {
+			clearInterval( obj.shake );
+			endFn && endFn();
+			obj.onOff = false;
+		}
+	}, 50);
+}
