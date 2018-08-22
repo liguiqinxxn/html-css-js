f7793d9455e613a61f16dce48d00c18a08fc1df5
lgq186 <1043736808@qq.com>
2018-08-22 19:21:43 +0800

    商品分类筛选和购物车案例

---
+++ 2-JS练习/练习/02-商品分类筛选查找/shoplist.js
@@ -0,0 +1,56 @@
+var shopsList = [
+	{
+		imgUrl:'',
+		size: '4.0-4.5英寸',
+		system: 'ios',
+		www:'联通3G',
+		name:'苹果',
+		volume:100000,
+		price: 5000
+	},
+	{
+		imgUrl:'',
+		size: '4.0-4.5英寸',
+		system: 'ios',
+		www:'双卡单4G',
+		name:'苹果',
+		volume:100,
+		price: 50000
+	},
+	{
+		imgUrl:'',
+		size: '4.6-4.9英寸',
+		system: 'ios',
+		www:'双卡单4G',
+		name:'苹果',
+		volume:30,
+		price: 50080
+	},
+	{
+		imgUrl:'',
+		size: '4.6-4.9英寸',
+		system: 'android',
+		www:'联通3G',
+		name:'小米',
+		volume:1020,
+		price: 50400
+	},
+	{
+		imgUrl:'',
+		size: '4.0-4.5英寸',
+		system: 'android',
+		www:'双卡单4G',
+		name:'小米',
+		volume:1040,
+		price: 54000
+	},
+	{
+		imgUrl:'',
+		size: '4.6-4.9英寸',
+		system: 'android',
+		www:'双卡单4G',
+		name:'小米',
+		volume:1004,
+		price: 50040
+	}
+]
